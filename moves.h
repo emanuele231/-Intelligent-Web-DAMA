9b72ea9ccab25fde4308c84591e703128aee3787
Emanuele <emanueledestena24@gmail.com>
2026-04-28 11:16:22 +0200

    definita base mosse

--- moves.h
+++ moves.h
@@ -1,2 +1,16 @@
 #ifndef MOVES_H
 #define MOVES_H
+
+/*MOVIMENTI
+
+1- Movimento pedine: in diagonale dx/sx
+2- Cattura: salto della pedina avversaria 
+    if (in diagonale sx/dx ce una pedina bianca)
+          salta pedina 
+          cancella pedina bianca
+          punti +1
+3- Movimento dama: if (pedina arriva al bordo opposto della tavola)
+                         diventa dama
+                         sblocca Movimento dxa sxa dxd sxd
+4- presa multipla: continua a mangiare se rileva pedine nel suo quadrante (diag)
+*/
